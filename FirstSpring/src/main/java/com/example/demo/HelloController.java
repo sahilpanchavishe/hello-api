296ecd19133e3c26b3344d72597470d36280fc1a
sahilpanchavishe <panchavishesahil@gmail.com>
2025-07-09 00:42:35 +0530

    Initial commit - Hello API

---
+++ FirstSpring/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse helloParam(@PathVariable String name){
+        return new HelloResponse("Hello " + name);
+    }
+
+    @GetMapping("/hello")
+    public HelloResponse hello(){
+        return new HelloResponse("Hello world");
+    }
+    @PostMapping("/hello")
+    public HelloResponse helloPost(@RequestBody String name){
+        return new HelloResponse("Hello " + name + " !");
+    }
+
+}
